1659e028ae25a4d584bb188fda85f42acc13b43f
Andrei Stoiculescu [Intern] <Andrei.Stoiculescu@endava.com>
2021-08-16 20:29:29 +0300

    ignored unncessary files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 #app = Flask(__name__, template_folder='templates/')
 app = Flask(__name__)
@@ -14,3 +14,7 @@ def html_name(page_name):
         return render_template(page_name + '.html')
     else:
         return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submitted succesfully!'
