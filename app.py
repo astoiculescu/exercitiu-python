a532042cc248d6863e2728a5262f98305bd58d45
Andrei Stoiculescu [Intern] <Andrei.Stoiculescu@endava.com>
2021-08-16 19:38:09 +0300

    working web app

--- app.py
+++ app.py
@@ -8,26 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('/index.html')
 
-@app.route("/index.html")
-def home():
-    return render_template('/index.html')
-
-@app.route("/about.html")
-def about():
-    return render_template('/about.html')
-
-@app.route("/components.html")
-def components():
-    return render_template('/components.html')
-
-@app.route("/contact.html")
-def contact():
-    return render_template('/contact.html')
-
-@app.route("/work.html")
-def work():
-    return render_template('/work.html')
-
-@app.route("/works.html")
-def works():
-    return render_template('/works.html')
+@app.route('/<string:page_name>')
+def html_name(page_name):
+    if('.html' not in page_name):
+        return render_template(page_name + '.html')
+    else:
+        return render_template(page_name)
